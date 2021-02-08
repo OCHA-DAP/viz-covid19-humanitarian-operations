6092c0c7d5a5d1f4f9725be8b181fae9f010c957
baripembo <erikawei@gmail.com>
2021-02-08 17:12:03 -0500

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -2073,11 +2073,17 @@ function handleGlobalEvents(layer) {
       features.forEach(function(feature) {
         if (feature.sourceLayer==adm0SourceLayer)
           target = feature;
-      });
+      });      
       if (target!=undefined) {
-        tooltip.setLngLat(e.lngLat);
-        if (target.properties.Terr_Name=='CuraÃ§ao') target.properties.Terr_Name = 'Curaçao';
-        createMapTooltip(target.properties.ISO_3, target.properties.Terr_Name, e.point);
+        if (target.properties.ISO_3!='BGD'&&target.properties.ISO_3!='BEN'&&target.properties.ISO_3!='COG'&&target.properties.ISO_3!='IRN'&&target.properties.ISO_3!='SLE'&&target.properties.ISO_3!='TGO'&&target.properties.ISO_3!='PRK') {
+          tooltip.setLngLat(e.lngLat);
+          if (target.properties.Terr_Name=='CuraÃ§ao') target.properties.Terr_Name = 'Curaçao';
+          tooltip.addTo(map);
+          createMapTooltip(target.properties.ISO_3, target.properties.Terr_Name, e.point);
+        }
+        else {
+          tooltip.remove();
+        }
       }
     }
   });
@@ -3148,7 +3154,7 @@ $( document ).ready(function() {
   var prod = (window.location.href.indexOf('ocha-dap')>-1 || window.location.href.indexOf('data.humdata.org')>-1) ? true : false;
   //console.log(prod);
 
-  mapboxgl.accessToken = 'pk.eyJ1IjoiaHVtZGF0YSIsImEiOiJja2hnbWs5NzkxMXh2MnNvcmF6dXIxMWE0In0.0GfmJoEJyWFQ5UzNxl2WgA';
+  mapboxgl.accessToken = 'pk.eyJ1IjoiaHVtZGF0YSIsImEiOiJja2FvMW1wbDIwMzE2MnFwMW9teHQxOXhpIn0.Uri8IURftz3Jv5It51ISAA';
   var tooltip = d3.select('.tooltip');
   var minWidth = 1000;
   viewportWidth = (window.innerWidth<minWidth) ? minWidth - $('.content-left').innerWidth() : window.innerWidth - $('.content-left').innerWidth();
