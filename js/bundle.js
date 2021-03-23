e36cf6e95a86a11decde48a73de022a5258f53b5
baripembo <erikawei@gmail.com>
2021-03-23 18:07:17 -0400

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -1995,7 +1995,8 @@ function createEvents() {
     selectLayer(this);
 
     //reset any deep links
-    window.history.replaceState(null, null, window.location.pathname);
+    var layer = $(this).attr('data-layer');
+    window.history.replaceState(null, null, window.location.pathname+'?layer='+layer);
   });
 
   //global figures close button
